c1c0617576babaf3f97faa630ab4539ff308744b
JK369x <104179568+JK369x@users.noreply.github.com>
2022-11-11 20:01:40 +0700

    f

--- search-filter/src/App.jsx
+++ search-filter/src/App.jsx
@@ -4,7 +4,8 @@ import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
-
+  const [word, setWord] = useState("");
+  const [dataFilter] = useState(["name", "capital"]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json()) //! res.json จะได้มาเป็น promiss
@@ -12,17 +13,44 @@ function App() {
         setCountries(data);
       });
   }, []);
-
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+  const searchCountries = (countries) => {
+    return countries.filter((item) => {
+      return dataFilter.some((filter) => {
+        if (item[filter]) {
+          if (filter === "name") {
+            return (
+              item[filter].common
+                .toString()
+                .toLowerCase()
+                .indexOf(word.toLowerCase()) > -1
+            );
+          } else {
+            item[filter].toString().toLowerCase().indexOf(word.toLowerCase()) >
+              -1;
+          }
+        }
+      });
+    });
+  };
   return (
     <div className="App">
       <div className="container">
         <div className="search-container">
           <label htmlFor="search-form">
-            <input type="text" className="search-input" placeholder="search..."/>
+            <input
+              type="text"
+              className="search-input"
+              placeholder="search..."
+              value={word}
+              onChange={(e) => setWord(e.target.value)}
+            />
           </label>
         </div>
         <ul className="row">
-          {countries.map((country, index) => {
+          {searchCountries(countries).map((country, index) => {
             return (
               <li key={index}>
                 <div className="card">
@@ -33,9 +61,16 @@ function App() {
                     <div className="card-description">
                       <h2>{country.name.common} </h2>
                       <ol className="card-list">
-                        <li>people :<span>{country.population}</span></li>
-                        <li>region : <span>{country.region}</span></li>
-                        <li>capital :<span>{country.capital}</span></li>
+                        <li>
+                          people :
+                          <span>{formatNumber(country.population)}</span>
+                        </li>
+                        <li>
+                          region : <span>{country.region}</span>
+                        </li>
+                        <li>
+                          capital :<span>{country.capital}</span>
+                        </li>
                       </ol>
                     </div>
                   </div>
